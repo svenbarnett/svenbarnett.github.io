af164ef60282ff50c261a16d51df206b9d9f2d4e
svenbarnett <svenbarnett@svenbarettdeAir.lan>
2022-01-01 15:41:00 +0800

    修复图片显示问题

--- themes/shoka/scripts/helpers/engine.js
+++ themes/shoka/scripts/helpers/engine.js
@@ -114,7 +114,11 @@ hexo.extend.helper.register('_image_url', function(img, path = '') {
   if (img.startsWith('//') || img.startsWith('http')) {
     return img
   } else {
-    return url_for.call(this, statics + (post_asset_folder ? path : '') + img)
+    var imgurl = url_for.call(this, statics + (post_asset_folder ? path : '') + img)
+    // 修复imgurl多了/导致找不到图片
+    imgurl = imgurl.replace("//", "/")
+    console.info && console.info(imgurl)
+    return imgurl
   }
 })
 
