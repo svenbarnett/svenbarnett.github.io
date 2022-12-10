686a10d7e3eef3454f559a0844444568a995a7ef
svenbarnett <3197544360@qq.com>
2022-12-10 14:55:31 +0800

    删除无用

--- .github/workflows/do_copy_pack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cp ./pack/hexo-asset-image/index.js ./node_modules/hexo-asset-image/index.js
-
-cp ./pack/hexo-generator-restful/lib/generator.js ./node_modules/hexo-generator-restful/lib/generator.js
-
-echo 'Copy Pack js ok!!!'
