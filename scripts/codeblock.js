a5635f4228032c1784ef9daa83125a79fd3b2ade
svenbarnett <3197544360@qq.com>
2023-03-12 20:14:49 +0800

    代码不可编辑

--- scripts/codeblock.js
+++ scripts/codeblock.js
@@ -3,7 +3,7 @@ var attributes = [
     'autocorrect="off"',
     'autocapitalize="off"',
     'spellcheck="false"',
-    'contenteditable="true"'
+    'contenteditable="false"'
   ]
   var attributesStr = attributes.join(' ')
   
